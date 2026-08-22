0c64d3b9131d473a6747c8d7dadc67c11a6fb344
CT-66 <css.com@protonmail.com>
2026-08-22 09:58:17 +0530

    Created Leetcode : 3622 - Check Divisibility by Digit Sum and Product.cpp

---
+++ Leetcode/3501-4000/3622 - Check Divisibility by Digit Sum and Product.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+  bool checkDivisibility(int n) {
+    int dSum = 0;
+    int dProd = 1;
+    int nn = n;
+
+    while (n > 0) {
+      dSum += (n % 10);
+      dProd *= (n % 10);
+      n /= 10;
+    }
+
+    if (nn % (dSum + dProd) == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
