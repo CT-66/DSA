e4b0537b3fe7f8839f2d8a6940eadf142fc80a32
CT-66 <css.com@protonmail.com>
2026-08-23 23:07:34 +0530

    Modified Leetcode: 9 - Palindrome Number.cpp

--- Leetcode/1-500/9 - Palindrome Number.cpp
+++ Leetcode/1-500/9 - Palindrome Number.cpp
@@ -1,16 +1,29 @@
 class Solution {
-public:
+  public:
     bool isPalindrome(int x) {
         string y = to_string(x);
+        // using string reverse function
         string z = y;
 
         reverse(z.begin(), z.end());
 
         if (y == z) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
+
+    // manual two pointer approach
+    /*
+    int l = 0, r = y.size() - 1;
+    while (l <= r) {
+        if (y[l] != y[r]) {
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
+    */
 };
