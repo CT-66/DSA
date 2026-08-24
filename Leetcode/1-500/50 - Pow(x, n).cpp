083e29bf6caba84e5de0c0e58d79b560f7b971d5
CT-66 <css.com@protonmail.com>
2026-08-25 05:23:38 +0530

    Modified Leetcode: 50 - Pow(x, n).cpp

--- Leetcode/1-500/50 - Pow(x, n).cpp
+++ Leetcode/1-500/50 - Pow(x, n).cpp
@@ -1,14 +1,13 @@
 class Solution {
-public:
-  double myPow(double x, int n) {
+  public:
+    double myPow(double x, int n) {
         long double res = 1;
         for (long double i = 1; i <= abs(n); i++) {
             res *= x;
         }
         if (n < 0) {
-    return (1 / res);
+            return (1 / res);
         }
         return res;
-
     }
 };
