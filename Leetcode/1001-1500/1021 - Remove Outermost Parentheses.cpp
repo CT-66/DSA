3d763d5e7bee72e5f44ba17466afe0def309a6e0
CT-66 <css.com@protonmail.com>
2026-08-27 16:11:37 +0530

    Modified Leetcode: 1021 - Remove Outermost Parentheses.cpp

--- Leetcode/1001-1500/1021 - Remove Outermost Parentheses.cpp
+++ Leetcode/1001-1500/1021 - Remove Outermost Parentheses.cpp
@@ -1,14 +1,38 @@
+/*
 class Solution {
-  public:
+public:
     string removeOuterParentheses(string S) {
         string res;
         int opened = 0;
         for (char c : S) {
-            if (c == '(' && opened++ > 0)
-                res += c;
-            if (c == ')' && opened-- > 1)
-                res += c;
+            if (c == '(' && opened++ > 0) res += c;
+            if (c == ')' && opened-- > 1) res += c;
         }
+        return res;
+    }
+};
+*/
+// more verbose
+class Solution {
+  public:
+    string removeOuterParentheses(string S) {
+        string res;
+        int depth = 0;
+
+        for (char c : S) {
+            if (c == '(') {
+                if (depth > 0)
+                    res += c;
+
+                depth++;
+            } else {
+                depth--;
+
+                if (depth > 0)
+                    res += c;
+            }
+        }
+
         return res;
     }
 };
