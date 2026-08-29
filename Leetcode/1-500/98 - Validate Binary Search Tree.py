5f7f773642c4544a78fa6c257415a28554504562
CT-66 <css.com@protonmail.com>
2026-08-29 14:02:09 +0530

    Created Leetcode: 98 - Validate Binary Search Tree.py

---
+++ Leetcode/1-500/98 - Validate Binary Search Tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def checkBST(root, minVal, maxVal):
+            if not root:
+                return True
+            if root.val <= minVal or root.val >= maxVal:
+                return False
+            return checkBST(root.left, minVal, root.val) and checkBST(
+                root.right, root.val, maxVal
+            )
+
+        return checkBST(root, -inf, inf)
